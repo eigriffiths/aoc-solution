d8658b2ed27b60df8b10877cf050b08bc59f84a5
Emyr Griffiths <emyr.griffiths@gmail.com>
2024-12-04 21:53:23 +0000

    part 2

--- src/2024/day01/part2.ts
+++ src/2024/day01/part2.ts
@@ -3,8 +3,15 @@ import { parseInputLines } from "../../../utils/fileReader.js";
 export function solve(input: string): number {
     const lines = parseInputLines(input);
 
-    const left = lines.map(item => Number(item.trim().split('   ')[0])).sort();
-    const right = lines.map(item => Number(item.trim().split('   ')[1])).sort();
+    const left: number[] = lines.map(item => Number(item.trim().split('   ')[0])).sort();
+    const right: number[] = lines.map(item => Number(item.trim().split('   ')[1])).sort();
 
-    return 1;
+    let similarity: number = 0; 
+    
+    left.map(item => {
+        const count: number = right.filter(rightItem => rightItem === item).length;
+
+        similarity += item * count;
+    })
+    return similarity;
 }
